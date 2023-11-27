5fc5ae851bc178702088f508499247913305def9
khshakilahamed <khshakil.ahamed18@gmail.com>
2023-11-28 02:00:13 +0600

    login sigup design updated

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,93 +1,134 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { HiOutlineMail } from 'react-icons/hi';
-import { RiLockPasswordFill } from 'react-icons/ri';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import useAuth from '../../hooks/useAuth';
-import facebook from './../../assets/icons/facebook.png';
-import google from './../../assets/icons/google+.png';
-import twitter from './../../assets/icons/twitter.png';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { HiOutlineMail } from "react-icons/hi";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import facebook from "./../../assets/icons/facebook.png";
+import google from "./../../assets/icons/google+.png";
+import twitter from "./../../assets/icons/twitter.png";
 
 const Login = () => {
-    const { error, signIn, signInWithGoogle } = useAuth();
-    const { register, formState: { errors }, handleSubmit, reset } = useForm();
-    const navigate = useNavigate();
-    const location = useLocation();
+  const { error, signIn, signInWithGoogle } = useAuth();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const from = location?.state?.from?.pathname || '/';
+  const from = location?.state?.from?.pathname || "/";
 
-    const handleSignIn = (data) => {
-        signIn({ ...data, navigate, from, reset });
-    }
+  const handleSignIn = (data) => {
+    signIn({ ...data, navigate, from, reset });
+  };
 
-    const handleGoogleSignIn = () => {
-        signInWithGoogle(navigate, from);
-    }
+  const handleGoogleSignIn = () => {
+    signInWithGoogle(navigate, from);
+  };
 
-    return (
-        <div className='flex justify-center items-center w-full' style={{ minHeight: '75vh' }}>
-            <div>
-                <h2 className='font-bold my-5 text-xl'>Sign in or create an account</h2>
-                <form onSubmit={handleSubmit(handleSignIn)}>
-                    <div className='border flex items-center p-2 w-96'>
-                        <label htmlFor="email">
-                            <HiOutlineMail className='mx-2 mr-5 cursor-pointer' />
-                        </label>
-                        <input
-                            className='w-full outline-none'
-                            type='email'
-                            id='email'
-                            placeholder='Email'
-                            {...register("email", { required: "Email address is required" })}
-                            aria-invalid={errors.email ? "true" : "false"}
-                        />
-                    </div>
-                    {errors.email && <p role="alert" className='text-red-500'>{errors.email?.message}</p>}
+  return (
+    <div
+      className="flex justify-center items-center w-full"
+      style={{ minHeight: "75vh" }}
+    >
+      <div>
+        <h2 className="font-bold my-5 text-xl">Sign in or create an account</h2>
+        <form onSubmit={handleSubmit(handleSignIn)}>
+          <div className="border flex items-center p-2 w-96">
+            <label htmlFor="email">
+              <HiOutlineMail className="mx-2 mr-5 cursor-pointer" />
+            </label>
+            <input
+              className="w-full outline-none"
+              type="email"
+              id="email"
+              placeholder="Email"
+              {...register("email", { required: "Email address is required" })}
+              aria-invalid={errors.email ? "true" : "false"}
+            />
+          </div>
+          {errors.email && (
+            <p role="alert" className="text-red-500">
+              {errors.email?.message}
+            </p>
+          )}
 
-                    <div className='border flex items-center p-2 w-96 mt-5'>
-                        <label htmlFor="password">
-                            <RiLockPasswordFill className='mx-2 mr-5 cursor-pointer' />
-                        </label>
-                        <input
-                            className='w-full outline-none'
-                            type="password"
-                            id="password"
-                            placeholder='Password'
-                            {...register("password", { required: "password is required" })}
-                            aria-invalid={errors.password ? "true" : "false"}
-                        />
+          <div className="border flex items-center p-2 w-96 mt-5">
+            <label htmlFor="password">
+              <RiLockPasswordFill className="mx-2 mr-5 cursor-pointer" />
+            </label>
+            <input
+              className="w-full outline-none"
+              type="password"
+              id="password"
+              placeholder="Password"
+              {...register("password", { required: "password is required" })}
+              aria-invalid={errors.password ? "true" : "false"}
+            />
+          </div>
+          {errors.password && (
+            <p role="alert" className="text-red-500">
+              {errors.password?.message}
+            </p>
+          )}
 
-                    </div>
-                    {errors.password && <p role="alert" className='text-red-500'>{errors.password?.message}</p>}
+          {error && (
+            <div className="alert alert-error shadow-lg mt-3 p-1 rounded-md">
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-current flex-shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span className="font-thin">Error! {error}</span>
+              </div>
+            </div>
+          )}
+          <p>
+            <Link to="/forgotPassword">
+              <small className="text-accent">Forgot Password?</small>
+            </Link>
+          </p>
+          <div className="mt-5 text-center w-96">
+            <input
+              className="btn w-full bg-accent text-white"
+              type="submit"
+              value="Login"
+            />
+          </div>
+        </form>
 
-                    {
-                        error && <div className="alert alert-error shadow-lg mt-3 p-1 rounded-md">
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                                <span className='font-thin'>Error! {error}</span>
-                            </div>
-                        </div>
-                    }
-                    <p>
-                        <Link to="/forgotPassword"><small className='text-accent'>Forgot Password?</small></Link>
-                    </p>
-                    <div className='mt-5 text-center w-96'>
-                        <input className='btn w-full bg-accent text-white' type="submit" value="Login" />
-                    </div>
-                </form>
-
-                <div className='flex flex-col items-center my-3'>
-                    <p>With your social network</p>
+        <div className="flex flex-col items-center my-3">
+          {/* <p>With your social network</p>
                     <div className='flex gap-4 my-2'>
                         <img className='cursor-pointer border p-1' src={facebook} alt="" />
                         <img onClick={handleGoogleSignIn} className='cursor-pointer border p-1' src={google} alt="" />
                         <img className='cursor-pointer border p-1' src={twitter} alt="" />
-                    </div>
-                    <p className='text-accent font-light'>Not a member? <Link to="/sign-up" className='cursor-pointer'>Sign up</Link></p>
-                </div>
-            </div>
+                    </div> */}
+          <p className="text-black font-light">
+            Not a member?{" "}
+            <Link
+              to="/sign-up"
+              className="cursor-pointer text-accent underline"
+            >
+              Sign up
+            </Link>
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,120 +1,163 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { FaUserAlt } from 'react-icons/fa';
-import { HiOutlineMail } from 'react-icons/hi';
-import { RiLockPasswordFill } from 'react-icons/ri';
-import { Link, useNavigate } from 'react-router-dom';
-import facebook from './../../assets/icons/facebook.png';
-import google from './../../assets/icons/google+.png';
-import twitter from './../../assets/icons/twitter.png';
-import PhoneInput from 'react-phone-number-input'
-import useAuth from '../../hooks/useAuth';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { FaUserAlt } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { Link, useNavigate } from "react-router-dom";
+import facebook from "./../../assets/icons/facebook.png";
+import google from "./../../assets/icons/google+.png";
+import twitter from "./../../assets/icons/twitter.png";
+import PhoneInput from "react-phone-number-input";
+import useAuth from "../../hooks/useAuth";
 
 const SignUp = () => {
-    const { error, registerUser, signInWithGoogle } = useAuth();
-    const { register, formState: { errors }, handleSubmit, reset } = useForm();
-    const navigate = useNavigate();
+  const { error, registerUser, signInWithGoogle } = useAuth();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
+  const navigate = useNavigate();
 
-    const handleSignUp = (data) => {
-        registerUser({ ...data, reset, navigate });
-        // console.log(data)
-    }
+  const handleSignUp = (data) => {
+    registerUser({ ...data, reset, navigate });
+    // console.log(data)
+  };
 
-    const handleGoogleSignIn = () => {
-        signInWithGoogle(navigate);
-    }
+  const handleGoogleSignIn = () => {
+    signInWithGoogle(navigate);
+  };
 
-    return (
-        <div className='flex justify-center items-center w-full' style={{ minHeight: '75vh' }}>
-            <div>
-                <h2 className='font-bold my-5 text-xl'>Create an account</h2>
-                <form onSubmit={handleSubmit(handleSignUp)}>
+  return (
+    <div
+      className="flex justify-center items-center w-full"
+      style={{ minHeight: "75vh" }}
+    >
+      <div>
+        <h2 className="font-bold my-5 text-xl">Create an account</h2>
+        <form onSubmit={handleSubmit(handleSignUp)}>
+          <div className="border flex items-center p-2 w-96">
+            <label htmlFor="name">
+              <FaUserAlt className="mx-2 mr-5 cursor-pointer" />
+            </label>
+            <input
+              className="w-full outline-none"
+              type="text"
+              id="name"
+              placeholder="Name"
+              {...register("name", { required: "Name is required" })}
+              aria-invalid={errors.name ? "true" : "false"}
+            />
+          </div>
+          {errors.name && (
+            <p role="alert" className="text-red-500">
+              {errors.name?.message}
+            </p>
+          )}
 
-                    <div className='border flex items-center p-2 w-96'>
-                        <label htmlFor="name">
-                            <FaUserAlt className='mx-2 mr-5 cursor-pointer' />
-                        </label>
-                        <input
-                            className='w-full outline-none'
-                            type='text'
-                            id='name'
-                            placeholder='Name'
-                            {...register("name", { required: "Name is required" })}
-                            aria-invalid={errors.name ? "true" : "false"}
-                        />
-                    </div>
-                    {errors.name && <p role="alert" className='text-red-500'>{errors.name?.message}</p>}
+          <div className="border flex items-center p-2 w-96 mt-5">
+            <PhoneInput
+              required
+              className="w-full outline-none"
+              id="phone"
+              country="BD"
+              placeholder="Mobile Number"
+              {...register("phone", { required: "Phone number is required" })}
+              aria-invalid={errors.phone ? "true" : "false"}
+            />
+          </div>
+          {errors.phone && (
+            <p role="alert" className="text-red-500">
+              {errors.phone?.message}
+            </p>
+          )}
 
-                    <div className='border flex items-center p-2 w-96 mt-5'>
-                        <PhoneInput
-                            required
-                            className='w-full outline-none'
-                            id='phone'
-                            country="BD"
-                            placeholder='Mobile Number'
-                            {...register("phone", { required: "Phone number is required" })}
-                            aria-invalid={errors.phone ? "true" : "false"}
-                        />
-                    </div>
-                    {errors.phone && <p role="alert" className='text-red-500'>{errors.phone?.message}</p>}
+          <div className="border flex items-center p-2 w-96 mt-5">
+            <label htmlFor="email">
+              <HiOutlineMail className="mx-2 mr-5 cursor-pointer" />
+            </label>
+            <input
+              className="w-full outline-none"
+              type="email"
+              id="email"
+              placeholder="Email"
+              {...register("email", { required: "Email address is required" })}
+              aria-invalid={errors.email ? "true" : "false"}
+            />
+          </div>
+          {errors.email && (
+            <p role="alert" className="text-red-500">
+              {errors.email?.message}
+            </p>
+          )}
 
-                    <div className='border flex items-center p-2 w-96 mt-5'>
-                        <label htmlFor="email">
-                            <HiOutlineMail className='mx-2 mr-5 cursor-pointer' />
-                        </label>
-                        <input
-                            className='w-full outline-none'
-                            type='email'
-                            id='email'
-                            placeholder='Email'
-                            {...register("email", { required: "Email address is required" })}
-                            aria-invalid={errors.email ? "true" : "false"}
-                        />
-                    </div>
-                    {errors.email && <p role="alert" className='text-red-500'>{errors.email?.message}</p>}
+          <div className="border flex items-center p-2 w-96 mt-5">
+            <label htmlFor="password">
+              <RiLockPasswordFill className="mx-2 mr-5 cursor-pointer" />
+            </label>
+            <input
+              className="w-full outline-none"
+              type="password"
+              id="password"
+              placeholder="Password"
+              {...register("password", { required: "password is required" })}
+              aria-invalid={errors.password ? "true" : "false"}
+            />
+          </div>
+          {errors.password && (
+            <p role="alert" className="text-red-500">
+              {errors.password?.message}
+            </p>
+          )}
 
-                    <div className='border flex items-center p-2 w-96 mt-5'>
-                        <label htmlFor="password">
-                            <RiLockPasswordFill className='mx-2 mr-5 cursor-pointer' />
-                        </label>
-                        <input
-                            className='w-full outline-none'
-                            type="password"
-                            id="password"
-                            placeholder='Password'
-                            {...register("password", { required: "password is required" })}
-                            aria-invalid={errors.password ? "true" : "false"}
-                        />
+          {error && (
+            <div className="alert alert-error shadow-lg mt-3 p-1 rounded-md">
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-current flex-shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span className="font-thin">Error! {error}</span>
+              </div>
+            </div>
+          )}
 
-                    </div>
-                    {errors.password && <p role="alert" className='text-red-500'>{errors.password?.message}</p>}
+          <div className="mt-5 text-center w-96">
+            <input
+              className="btn w-full bg-accent text-white"
+              type="submit"
+              value="Sign up"
+            />
+          </div>
+        </form>
 
-                    {
-                        error && <div className="alert alert-error shadow-lg mt-3 p-1 rounded-md">
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                                <span className='font-thin'>Error! {error}</span>
-                            </div>
-                        </div>
-                    }
-
-                    <div className='mt-5 text-center w-96'>
-                        <input className='btn w-full bg-accent text-white' type="submit" value="Sign up" />
-                    </div>
-                </form>
-
-                <div className='flex flex-col items-center my-3'>
-                    <p>With your social network</p>
+        <div className="flex flex-col items-center my-3">
+          {/* <p>With your social network</p>
                     <div className='flex gap-4 my-2'>
                         <img className='cursor-pointer border p-1' src={facebook} alt="" />
                         <img  onClick={handleGoogleSignIn}  className='cursor-pointer border p-1' src={google} alt="" />
                         <img className='cursor-pointer border p-1' src={twitter} alt="" />
-                    </div>
-                    <p className='text-accent font-light'>Already have an account? <Link to='/login' className='cursor-pointer'>login</Link></p>
-                </div>
-            </div>
+                    </div> */}
+          <p className="text-black font-light">
+            Already have an account?{" "}
+            <Link to="/login" className="cursor-pointer underline text-accent">
+              login
+            </Link>
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
